6bc9591b4b29415c158b2758d4f68c01745cc096
William Bigas Mauro <williambmauro@hotmail.com>
2022-03-11 11:27:25 -0300

    Adicionado primeiro teste usando mockito

---
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,63 @@
+package br.com.alura.leilao.service;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FinalizarLeilaoServiceTest {
+
+    private FinalizarLeilaoService service;
+
+    @Mock
+    private LeilaoDao leilaoDao;
+
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+        this.service = new FinalizarLeilaoService(leilaoDao);
+    }
+
+    @Test
+    void deveriaFinalizarUmLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        Mockito.when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+
+        service.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get(0);
+        assertTrue(leilao.isFechado());
+        assertEquals(new BigDecimal(900) , leilao.getLanceVencedor().getValor());
+        Mockito.verify(leilaoDao).salvar(leilao);
+    }
+
+    private List<Leilao> leiloes() {
+        List<Leilao> lista = new ArrayList<>();
+
+        Leilao leilao = new Leilao("Celular" , new BigDecimal("500"), new Usuario("Fulano"));
+
+        Lance primeiro = new Lance(new Usuario("Beltrano") , new BigDecimal(600));
+        Lance segundo = new Lance(new Usuario("Ciclano") , new BigDecimal(900));
+
+        leilao.propoe(primeiro);
+        leilao.propoe(segundo);
+
+        lista.add(leilao);
+        return lista;
+    }
+
+}
