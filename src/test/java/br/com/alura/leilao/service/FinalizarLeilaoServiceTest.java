344ae314579a9e36fe4cc7f52293d16fb2bc7c56
William Bigas Mauro <williambmauro@hotmail.com>
2022-03-11 12:01:19 -0300

    Adicionado teste de exception

--- src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -60,6 +60,21 @@ class FinalizarLeilaoServiceTest {
         Mockito.verify(enviadorDeEmails).enviarEmailVencedorLeilao(lanceVencedor);
     }
 
+    @Test
+    void naoDeveriaEnviarEmailParaVencedorDoLeilaoEmCasoDeErroAoEncerrarOleilao() {
+        List<Leilao> leiloes = leiloes();
+
+        Mockito.when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+        Mockito.when(leilaoDao.salvar(Mockito.any())).thenThrow(RuntimeException.class);
+
+        try {
+            service.finalizarLeiloesExpirados();
+            Mockito.verifyNoInteractions(enviadorDeEmails);
+        } catch (Exception e) {
+
+        }
+    }
+
     private List<Leilao> leiloes() {
         List<Leilao> lista = new ArrayList<>();
 
